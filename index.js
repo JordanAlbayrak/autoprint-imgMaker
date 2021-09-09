9aa26e0bcbfeebf6ebe7f707deef29573f829bcf
Jordan Alba <jordan008@live.ca>
2021-09-09 15:21:16 -0400

    Please god let it work

--- index.js
+++ index.js
@@ -61,53 +61,51 @@ const LINE_BREAK = 16;
 
 async function createImage(CUSTOMER_DATA) {
 
-    return  new Jimp(256, 256, "white", async (err, image) => {
+    const image = new Jimp(256, 256, "white");
+    const [font, fontSmall, logo, bar] = await Promise.all([
+        Jimp.loadFont(Jimp.FONT_SANS_16_BLACK),
+        Jimp.loadFont(Jimp.FONT_SANS_12_BLACK),
+        Jimp.read("logo.jpg"),
+        new Jimp(236, 5, "black"),
+    ]);
+
+    image.composite(logo.resize(64, 64), 256-64, 0);
+
+    // Composite black line on label
+    image.composite(bar, 10, 90);
+
+    // Write FROM on label
+    writeCoolText(
+        image,
+        fontSmall,
+        "FROM: 2705 CROIS Marquise\nBrossard, J4Y 1P1\nQuebec, Canada",
+        10,
+        32,
+        256,
+        256,
+        12,
+    );
+
+    const lines = getCustomer(CUSTOMER_DATA).split`\n`;
+    let start = 200 - lines.length * LINE_BREAK;
+
+    // Write customer info on label
+    writeCoolText(
+        image,
+        font,
+        getCustomer(CUSTOMER_DATA),
+        0,
+        start,
+        256,
+        256,
+        16,
+        {
+            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+            alignmentY: Jimp.VERTICAL_ALIGN_CENTER,
+        },
+    );
     
-        const [font, fontSmall, logo, bar] = await Promise.all([
-            Jimp.loadFont(Jimp.FONT_SANS_16_BLACK),
-            Jimp.loadFont(Jimp.FONT_SANS_12_BLACK),
-            Jimp.read("logo.jpg"),
-            new Jimp(236, 5, "black"),
-        ]);
-    
-        image.composite(logo.resize(64, 64), 256-64, 0);
-    
-        // Composite black line on label
-        image.composite(bar, 10, 90);
-    
-        // Write FROM on label
-        writeCoolText(
-            image,
-            fontSmall,
-            "FROM: 2705 CROIS Marquise\nBrossard, J4Y 1P1\nQuebec, Canada",
-            10,
-            32,
-            256,
-            256,
-            12,
-        );
-    
-        const lines = getCustomer(CUSTOMER_DATA).split`\n`;
-        let start = 200 - lines.length * LINE_BREAK;
-    
-        // Write customer info on label
-        writeCoolText(
-            image,
-            font,
-            getCustomer(CUSTOMER_DATA),
-            0,
-            start,
-            256,
-            256,
-            16,
-            {
-                alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-                alignmentY: Jimp.VERTICAL_ALIGN_CENTER,
-            },
-        );
-        
-        return image;
-    });
+    return image;
     
 }
 function getCustomer({ shipping_address: { first_name: firstName, last_name: lastName, address1: address, city, zip, province, country } }) {
